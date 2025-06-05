5e3df53f68be015f1ba4dfa92ea4137420f9ffbf
Dante Nicola Capobianco <dantecapobianco@meta.com>
2025-06-05 08:01:05 -0700

    Generate playlist JSON  data to populate music list

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,11 +3,15 @@ const selectedPlaylist = document.getElementsByClassName("playlist-cards")[0];
 const modal = document.getElementById("playlist-modal");
 const closeBtn = document.getElementById("close-btn");
 
+document.addEventListener("DOMContentLoaded", function () {
+   // dynamic display logic here
+});
+
 closeBtn.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
-window.addEventListener("click",function (event) {
+window.addEventListener("click", function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -15,16 +19,16 @@ window.addEventListener("click",function (event) {
 
 function openModal(playlistTitle) {
   // const foundPlaylist = ...
-//   document.getElementById("festivalName").innerText = festival.name;
-//   document.getElementById("festivalImage").src = festival.imageUrl;
-//   document.getElementById(
-//     "festivalDates"
-//   ).innerText = `Dates: ${festival.dates}`;
-//   document.getElementById(
-//     "festivalLocation"
-//   ).innerText = `Location: ${festival.location}`;
-//   document.getElementById(
-//     "artistLineup"
-//   ).innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(", ")}`;
+  //   document.getElementById("festivalName").innerText = festival.name;
+  //   document.getElementById("festivalImage").src = festival.imageUrl;
+  //   document.getElementById(
+  //     "festivalDates"
+  //   ).innerText = `Dates: ${festival.dates}`;
+  //   document.getElementById(
+  //     "festivalLocation"
+  //   ).innerText = `Location: ${festival.location}`;
+  //   document.getElementById(
+  //     "artistLineup"
+  //   ).innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(", ")}`;
   modal.style.display = "block";
 }
