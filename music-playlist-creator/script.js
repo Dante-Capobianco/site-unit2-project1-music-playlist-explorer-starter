2d1af5a1e8f1f87250c221b9168fa4429b740e88
Dante Nicola Capobianco <dantecapobianco@meta.com>
2025-06-05 07:31:52 -0700

    Create modal overlay accessible by clicking a playlist tile

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,30 @@
+// JavaScript for Opening and Closing the Modal
+const selectedPlaylist = document.getElementsByClassName("playlist-cards")[0];
+const modal = document.getElementById("playlist-modal");
+const closeBtn = document.getElementById("close-btn");
 
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click",function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+function openModal(playlistTitle) {
+  // const foundPlaylist = ...
+//   document.getElementById("festivalName").innerText = festival.name;
+//   document.getElementById("festivalImage").src = festival.imageUrl;
+//   document.getElementById(
+//     "festivalDates"
+//   ).innerText = `Dates: ${festival.dates}`;
+//   document.getElementById(
+//     "festivalLocation"
+//   ).innerText = `Location: ${festival.location}`;
+//   document.getElementById(
+//     "artistLineup"
+//   ).innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(", ")}`;
+  modal.style.display = "block";
+}
